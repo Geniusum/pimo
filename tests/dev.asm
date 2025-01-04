6faf2e34bd372e717ac9014f30d8a22a222d2083
Genius_um <genius_um@debian>
2025-01-04 19:37:26 +0100

    Commit

--- tests/dev.asm
+++ tests/dev.asm
@@ -1,41 +1,26 @@
 format ELF64 executable 3
 segment readable executable
-	; Ini instruction
-	; Added to memory 'mem_E8664B30' the element 'x' of type u8<1>
-	mov rsi, stack_DD7D40D2
-	; Started stack 'stack_DD7D40D2' of size 128
+	; Exit instruction
+	mov rsi, stack_DED7A35C
+	; Started stack 'stack_DED7A35C' of size 128
 	mov byte [rsi], 4
 	add rsi, 1
-	mov rsi, mem_E8664B30
-	mov rax, stack_DD7D40D2
-	mov al, byte [rax]
-	mov byte [rsi], al
-	add rsi, 1
-	; Exit instruction
-	mov rsi, stack_FD70A203
-	; Started stack 'stack_FD70A203' of size 128
-	mov rax, mem_E8664B30
-	mov al, byte [rax]
-	mov byte [rsi], al
-	add rsi, 1
-	mov rax, mem_E8664B30
-	mov al, byte [rax]
-	mov byte [rsi], al
+	mov byte [rsi], 3
 	add rsi, 1
 	; Add stack operator
-	mov rbx, stack_FD70A203
-	mov rcx, stack_FD70A203
+	mov rbx, stack_DED7A35C
+	mov rcx, stack_DED7A35C
 	sub rsi, 1
 	movzx rbx, byte [rsi]
 	sub rsi, 1
 	movzx rcx, byte [rsi]
 	add rbx, rcx
 	mov byte [rsi], bl
-	mov byte [rsi], 4
+	mov byte [rsi], 5
 	add rsi, 1
 	; Add stack operator
-	mov rbx, stack_FD70A203
-	mov rcx, stack_FD70A203
+	mov rbx, stack_DED7A35C
+	mov rcx, stack_DED7A35C
 	sub rsi, 1
 	movzx rbx, byte [rsi]
 	sub rsi, 1
@@ -43,12 +28,10 @@ segment readable executable
 	add rbx, rcx
 	mov byte [rsi], bl
 	mov rax, 60
-	mov rsi, stack_FD70A203
+	mov rsi, stack_DED7A35C
 	movzx rdi, byte [rsi]
 	syscall 
-	mov rsi, stack_DD7D40D2
-	mov rsi, stack_FD70A203
+	mov rsi, stack_DED7A35C
 segment readable writeable
-	mem_E8664B30 rb 512
-	stack_DD7D40D2 rb 128
-	stack_FD70A203 rb 128
+	mem_C8727BCB rb 512
+	stack_DED7A35C rb 128
