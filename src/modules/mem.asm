5432c4938f498618b22f17bd002bca4d99b1bdf0
Genius_um <genius_um@debian>
2025-01-17 16:16:16 +0100

    Commit

--- src/modules/mem.asm
+++ src/modules/mem.asm
@@ -4,11 +4,11 @@
 
 ALLOC 0x668A0, 98000  ; from 0x668A0 to 0x7E770 : Memories scope
 ; The memory scope will had all memory names and their addresses
-; name 0x2C addr 0x3B ...
+; name addr ...
 
 ALLOC 0x7E771, 147000 ; from 0x7E771 to 0xA25A9 : Variables scope
 ; The memory scope will had all variables names, sizes, lenght, address and parent memory
-; name 0x2C mem 0x2C size 0x2C len 0x2C addr 0x3B ...
+; name mem size len addr ...
 
 SCOPE_MEM = 0x668A0
 SCOPE_MEM_SIZE = 98000
@@ -16,3 +16,14 @@ SCOPE_VAR_END = 0x7E770
 SCOPE_VAR = 0x7E771
 SCOPE_VAR_SIZE = 147000
 SCOPE_VAR_END = 0xA25A9
+
+; varnames & memnames on 64 bytes
+; size on 1 byte
+; lenght on 4 bytes
+; address on 8 bytes
+
+GET_LAST
+
+ADD_VAR name, memname, size, lenght, address {
+
+}
