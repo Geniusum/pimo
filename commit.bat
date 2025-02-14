18ec1c4775fee9582519866be2e15adfa43e6ee1
Genius_um <geniusum.off@gmail.com>
2025-02-14 15:51:20 +0100

    Update commit.bat

--- commit.bat
+++ commit.bat
@@ -1,3 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%~1"=="" (
+    set "commit_message=Commit"
+) else (
+    set "commit_message=%~1"
+)
+
 git add .
-git commit -m "Commit"
+git commit -m "!commit_message!"
 git push origin
